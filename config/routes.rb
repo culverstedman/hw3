cbb653a53b0ae5c27d12897498bf297704351e14
culverstedman <culver.stedman@kellogg.northwestern.edu>
2026-02-24 04:46:16 +0000

    added post commands to the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   
   get("/places", {:controller => "places", :action => "index" })
   get("/places/new", {:controller => "places", :action => "new" })
+  post("/places", {:controller => "places", :action => "create" })
   get("/places/:id", {:controller => "places", :action => "show" })
 
   get("/entries/new", {:controller => "entries", :action => "new" })
-
+  post("/entries", {:controller => "entries", :action => "create" })
 end
