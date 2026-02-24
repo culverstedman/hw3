eb0326c326583526a4450e311fbbcc04a9eec04a
culverstedman <culver.stedman@kellogg.northwestern.edu>
2026-02-24 04:19:34 +0000

    added routes with get commands

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+  
+  get("/", { :controller => "places", :action => "index" })
+  
+  get("/places", {:controller => "places", :action => "index" })
+  get("/places/new", {:controller => "places", :action => "new" })
+  get("/places/:id", {:controller => "places", :action => "show" })
+
+  get("/entries/new", {:controller => "entries", :action => "new" })
+
 end
